416da3a1faed2e307e1e4af7c5bc16f118ecbdc1
qqyoungqq <qianyang@mail.usf.edu>
2015-07-23 22:44:57 -0400

    feat: Allow google map to show varying neighborhood

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,73 @@
-function initialize() {
-	var neighborhood = 'Coral Gables, FL';
+/* Model */
+var initialNeighborhood = 'Miami, FL';
 
-    var request = {
-    	   query: neighborhood
-    };
+/* ViewModel */
+var ViewModel = function() {
+    var self=this;
+    var map;
+    var service;
 
-	var mapOptions = {
-    	zoom: 14,
-    	disableDefaultUI: true
-	};
+    this.neighborhood = ko.observable(initialNeighborhood);
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-    var infowindow = new google.maps.InfoWindow();
+    function initialMap() {
+        var mapOptions = {
+            zoom: 14,
+            disableDefaultUI: true
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+    }
 
-    var service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callbackNeighborhood);
-	
+    initialMap();
+
+    function searchNeighborhood(neighborhood) {
+        var request = {
+            query: neighborhood
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, callbackNeighborhood);
+    }
+
+    this.resetNeighborhood = ko.computed(function() {
+        if (self.neighborhood() != '' ) {
+            searchNeighborhood(self.neighborhood());
+        }
+    });
+
     function callbackNeighborhood(results, status) {
-    	if (status == google.maps.places.PlacesServiceStatus.OK) {
-    		getNeighborhoodInformation(results[0])
-    	}
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            showNeighborhood(results[0]);
+        }
     }
 
-    function getNeighborhoodInformation(placeData) {
- 		var lat = placeData.geometry.location.lat();  // latitude from the place service
-    	var lon = placeData.geometry.location.lng();  // longitude from the place service
-    	var name = placeData.formatted_address;   	  // name of the place from the place service
-    	var neighCenter = new google.maps.LatLng(lat,lon);
-    	map.setCenter(neighCenter);	
+    function showNeighborhood(placeData) {
+        var lat = placeData.geometry.location.lat();    // latitude from the place service
+        var lon = placeData.geometry.location.lng();    // longitude from the place service
+        var name = placeData.formatted_address;         // name of the place from the place service
+        var bounds = new google.maps.LatLngBounds();    // create a LatLngBounds object
+        var neighCenter = new google.maps.LatLng(lat,lon);
+
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            title: name,
+            icon: {
+            // Star
+                path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
+                fillColor: '#ffff00',
+                fillOpacity: 1,
+                scale: 1/2,
+                strokeColor: '#bd8d2c',
+                strokeWeight: 1
+            }
+        });
+
+        // extend the bound to contain the given point
+        bounds.extend(placeData.geometry.location);
+        // center the map
+        map.setCenter(neighCenter); 
+
     }
 
-  window.mapBounds = new google.maps.LatLngBounds();
-}
+};
 
-google.maps.event.addDomListener(window,'load',initialize);
-
-window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-   map.fitBounds(mapBounds);
-});
+ko.applyBindings(new ViewModel())
