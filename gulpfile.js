1459fcd980d7864e0ae49cb1ac15a678f948f36f
qqyoungqq <qianyang@mail.usf.edu>
2015-08-21 13:56:41 -0400

    doc: Output minified codes to master branch and then copy to the gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,41 @@ var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
 	uglify = require('gulp-uglify');
 
-// Minifies our javaScript file and output it to build/js/*.html
 gulp.task('minify-script',function() {
 	gulp.src('src/js/app.js')
 		.pipe(uglify())
-		.pipe(gulp.dest('build/js/'));
+		.pipe(gulp.dest('../master/js/'));
 });
 
 gulp.task('minify-lib', function() {
 	gulp.src('src/js/lib/*.js')
 		.pipe(uglify())
-		.pipe(gulp.dest('build/js/lib/'));
+		.pipe(gulp.dest('../master/js/lib/'));
 })
 
 gulp.task('minify-style', function(){
 	gulp.src('src/css/style.css')
 		.pipe(minifyCSS())
-		.pipe(gulp.dest('build/css/'));
+		.pipe(gulp.dest('../master/css/'));
 });
 
 gulp.task('minify-html', function(){
 	gulp.src('src/index.html')
 		.pipe(minifyHTML())
-		.pipe(gulp.dest('build/'));
+		.pipe(gulp.dest('../master/'));
+});
+
+gulp.task('copy', function() {
+    gulp.src(['../master/js/*'])
+        .pipe(gulp.dest('../gh-pages/js/'));
+    gulp.src(['../master/js/lib/*'])
+        .pipe(gulp.dest('../gh-pages/js/lib/'));        
+    gulp.src(['../master/css/*'])
+        .pipe(gulp.dest('../gh-pages/css/'));
+    gulp.src('../master/*.html')
+        .pipe(gulp.dest('../gh-pages/'));
+    gulp.src('../master/README.md')
+        .pipe(gulp.dest('../gh-pages/'));
 });
 
 gulp.task('watch',function() {
@@ -33,6 +45,7 @@ gulp.task('watch',function() {
 	gulp.watch('src/js/lib/*.js',['minify-lib']);
 	gulp.watch('src/css/*.css',['minify-style']);
 	gulp.watch('src/css/*.html',['minify-html']);
+	gulp.watch('../master/*',['copy']);
 });
 
-gulp.task('default',['minify-html','minify-style','minify-script','minify-lib','watch']);
+gulp.task('default',['minify-html','minify-style','minify-script','minify-lib','copy','watch']);
